87cae61484f560fe4735f73fdf50963287496f63
Zdeněk Sojma <zdenek@sojma.cz>
2023-12-09 14:06:57 +0100

    remove of dist cache

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,11 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-});
+builder.Services.AddDistributedMemoryCache();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
